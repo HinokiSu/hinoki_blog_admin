19e142d832a3af4d5e98bdd1cd63af7efeb1d437
Hinoki <yukihinoki@qq.com>
2022-01-17 22:24:19 +0800

    feat: add create and update logic

--- src/views/article/article-form.vue
+++ src/views/article/article-form.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="hinoki-blog preview-container">
-    <fe-form :model="formValue" :rules="rules" ref="formRef">
+    <fe-form :model="formValue" :rules="rules" ref="formRef" show-message>
       <fe-form-item prop="title">
         <div class="title">
           <h1>Title</h1>
@@ -10,14 +10,20 @@
       <fe-form-item prop="description">
         <div class="description">
           <h1>Description</h1>
-          <textarea class="desc-textarea"></textarea>
+          <textarea class="desc-textarea" v-model="formValue.description"></textarea>
+        </div>
+      </fe-form-item>
+      <!-- TODO: .md -> pre -> html -->
+      <fe-form-item>
+        <div class="markdown-html">
+          <h1>MarkDown</h1>
+          <textarea v-model="formValue.markdown"></textarea>
         </div>
       </fe-form-item>
       <fe-form-item>
         <div class="markdown-html">
-          <h1>MarkDown</h1>
-          <textarea></textarea>
-          <!-- TODO: .md -> pre -> html -->
+          <h1>Html</h1>
+          <textarea v-model="formValue.html"></textarea>
         </div>
       </fe-form-item>
 
@@ -38,9 +44,10 @@
 
 <script lang="ts">
 import { IArticle } from '@admin/interfaces'
+import router from '@admin/routes'
 import { useArticleStore } from '@admin/stores/articleStore'
-import { computed, defineComponent, onBeforeUnmount, PropType, ref, watch, watchEffect } from 'vue'
-import { onBeforeRouteLeave, onBeforeRouteUpdate, useRoute } from 'vue-router'
+import { computed, defineComponent, getCurrentInstance, onBeforeUnmount, ref, watchEffect } from 'vue'
+import { useRoute } from 'vue-router'
 export default defineComponent({
   name: 'ArticleForm',
   setup() {
@@ -48,43 +55,76 @@ export default defineComponent({
 
     const articleStore = useArticleStore()
     const route = useRoute()
-
+    const { proxy } = getCurrentInstance() as any
     const articleId = <string | undefined>route.params?.id
+    const formValue = computed<IArticle>(() => articleStore.articleData)
+
+    let sumbitHandler
 
     if (articleId) {
       // /article/update/:id
-      const fetchData = (id: string) => {
+      const fetchData = async (id: string) => {
         // get data -> filling articleData of articleStore
-        articleStore.getArticleById(id)
+        await articleStore.getArticleById(id)
       }
 
-      watchEffect(() => {
-        fetchData(articleId as string)
+      watchEffect( async () => {
+        await fetchData(articleId as string)
       })
+
+      sumbitHandler = async () => {
+        // articleStore -> updateArticle
+        await articleStore.updateArticle(articleId)
+        console.log(articleStore.fettle)
+        if (articleStore.fettle) {
+          articleStore.fettle = false
+          router.push({
+            name: 'articles',
+          })
+        } else {
+          proxy.$toast['error']({
+            text: 'Update failed!',
+            duration: '2000',
+          })
+        }
+      }
+    } else {
+      sumbitHandler = async() => {
+        // articleStore -> createArticle
+        await articleStore.createArticle()
+
+        if (articleStore.fettle) {
+          articleStore.fettle = false
+          // TODO: after submit successfully, giving some tips
+          router.push({
+            name: 'articles',
+          })
+        } else {
+          proxy.$toast['error']({
+            text: 'Create failed!',
+            duration: '2000',
+          })
+        }
+      }
     }
 
-    const formValue = computed<IArticle>(() => articleStore.articleData)
-
     const rules = {
+      // TODO: validate form
       title: [
         {
           required: true,
+          type: 'string',
           trigger: 'blur',
         },
       ],
     }
 
+    const cancelHandler = () => {}
+
     // recycle
     onBeforeUnmount(() => {
       articleStore.articleData = {}
     })
-
-    const sumbitHandler = () => {
-      // articleStore -> httpPost
-      console.log(formValue.value)
-    }
-
-    const cancelHandler = () => {}
 
     return {
       formValue,

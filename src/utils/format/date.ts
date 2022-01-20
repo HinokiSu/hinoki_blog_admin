c91d8353b3049d7c2a779e3dc8c2f9e87606f9d4
Hinoki <yukihinoki@qq.com>
2022-01-20 20:01:18 +0800

    chore: update date formatter

--- src/utils/format/date.ts
+++ src/utils/format/date.ts
@@ -1,12 +1,10 @@
-// default import not work, issue: https://github.com/aurelia/skeleton-navigation/issues/606
-import moment from 'moment'
+import dayjs from 'dayjs'
 
 /**
  * format date
- * @param rawDate raw date resource
  * @param format Converted format
  * @returns formated date
  */
-export function dateFormat(rawDate: Date, format: string): string {
-  return <string>moment(rawDate).format(format)
+export function nowDateFormat(format: string): string {
+  return <string>dayjs().format(format)
 }

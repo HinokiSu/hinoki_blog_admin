6b4cdab17ced8be3e74efddce12ea86df5ac49d1
Hinoki <yukihinoki@qq.com>
2022-01-21 20:50:15 +0800

    fix: fix import errors

--- src/layouts/content-layout.vue
+++ src/layouts/content-layout.vue
@@ -12,13 +12,12 @@
 
 <script lang="ts">
 import { defineComponent, ref } from 'vue'
-import CardBlock from '@admin/components/main-panel/card-block/card-block.vue'
-import CardBreadnav from '@admin/components/main-panel/card-main/card-breadnav.vue'
+import CardBreadnav from '@admin/components/card-main/card-breadnav.vue'
 import { IBreadNav } from '@admin/interfaces'
 
 export default defineComponent({
   name: 'ContentLayout',
-  components: { CardBlock, CardBreadnav },
+  components: { CardBreadnav },
   setup() {
     const navList = <IBreadNav[]>[
       {
--- src/views/article/index.vue
+++ src/views/article/index.vue
@@ -5,9 +5,9 @@
     </div>
     <fe-grid-group direction="row" class="block-group">
       <fe-grid class="article_card_block" v-for="article in articleList" :key="article._id">
-        <card-block :id="article._id" to-link-name="update-article">
+        <article-features :id="article._id" to-link-name="update-article">
           <article-content :article="article"></article-content>
-        </card-block>
+        </article-features>
       </fe-grid>
     </fe-grid-group>
   </div>
@@ -15,14 +15,14 @@
 
 <script lang="ts">
 import { computed, defineComponent, onMounted } from 'vue'
-import cardBlock from '@admin/components/main-panel/card-block/card-block.vue'
-import { useArticleStore } from '@admin/stores/articleStore'
-import ArticleContent from '@admin/components/main-panel/card-block/article-content.vue'
-import CardFeature from '@admin/components/main-panel/card-main/card-feature.vue'
 import router from '@admin/routes'
+import { useArticleStore } from '@admin/stores/articleStore'
+import ArticleContent from '@admin/components/article-item/article-content.vue'
+import CardFeature from '@admin/components/card-main/card-feature.vue'
+import ArticleFeatures from '@admin/components/article-item/article-features.vue'
 export default defineComponent({
   name: 'Articles',
-  components: { cardBlock, ArticleContent, CardFeature },
+  components: { ArticleContent, CardFeature, ArticleFeatures },
   setup() {
     const ArticleStore = useArticleStore()
     onMounted(() => {

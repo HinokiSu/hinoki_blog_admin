8cd312ced7470931b17aedcd52c86293c1554849
Hinoki <yukihinoki@qq.com>
2022-02-11 20:02:20 +0800

    feat: add tips of empty wrap

---
+++ src/components/empty-wrap/index.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="hinoki-blog empty-wrap" v-if="isEmpty">
+    <div class="tips">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'EmptyWrap',
+  props: {
+    isEmpty: Boolean,
+    tips: String,
+  },
+  setup() {
+    return {}
+  },
+})
+</script>
+
+<style lang="less" scoped>
+.hinoki-blog {
+  & .empty-wrap {
+    width: 100%;
+    height: 50vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    .tips {
+      font-size: 36px;
+      font-weight: 500;
+    }
+  }
+}
+</style>

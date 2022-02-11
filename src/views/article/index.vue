364fffe100b4e0f6447800930c8b2ce4e3f75cc9
Hinoki <yukihinoki@qq.com>
2022-02-11 20:08:33 +0800

    feat: add and use empty wrap component

--- src/views/article/index.vue
+++ src/views/article/index.vue
@@ -10,21 +10,27 @@
         </article-features>
       </fe-grid>
     </fe-grid-group>
+    <empty-wrap :is-empty="isNullArticleList">
+        <p>没有任何文章 : (</p>
+    </empty-wrap>
   </div>
 </template>
 
 <script lang="ts">
-import { computed, defineComponent, onMounted } from 'vue'
+import { computed, defineComponent, onMounted, ref, watch, watchEffect } from 'vue'
 import router from '@admin/routes'
 import { useArticleStore } from '@admin/stores/articleStore'
 import ArticleContent from '@admin/components/article-item/article-content.vue'
 import CardFeature from '@admin/components/card-main/card-feature.vue'
 import ArticleFeatures from '@admin/components/article-item/article-features.vue'
+import EmptyWrap from '@admin/components/empty-wrap/index.vue'
 export default defineComponent({
   name: 'Articles',
-  components: { ArticleContent, CardFeature, ArticleFeatures },
+  components: { ArticleContent, CardFeature, ArticleFeatures, EmptyWrap },
   setup() {
     const ArticleStore = useArticleStore()
+    const isNullArticleList = ref(false)
+
     onMounted(() => {
       ArticleStore.getArticleList()
     })
@@ -38,11 +44,22 @@ export default defineComponent({
         name: 'create-article',
       })
     }
+    const articleList = computed(() => ArticleStore.articleList)
+
+    watch(
+      () => ArticleStore.articleList,
+      () => {
+        if (ArticleStore.articleCount === 0) {
+          isNullArticleList.value = true
+        }
+      }
+    )
 
     return {
-      articleList: computed(() => ArticleStore.articleList),
+      articleList,
       searcHandler,
       addjuctionHandler,
+      isNullArticleList,
     }
   },
 })

f6f00068667653b35b7b512c5ec5f0138d225a41
Hinoki <yukihinoki@qq.com>
2022-03-07 12:55:42 +0800

    feat: use nprogress

--- src/utils/axios/http.ts
+++ src/utils/axios/http.ts
@@ -1,6 +1,10 @@
 import router from '@admin/routes'
 import axios from 'axios'
 import { useUserStore } from '@admin/stores/userStore'
+
+import NProgress from 'nprogress'
+import 'nprogress/nprogress.css'
+
 axios.defaults.baseURL = '/api/admin'
 axios.defaults.timeout = 10000
 // Request header information is set for post request
@@ -9,6 +13,7 @@ axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded
 // 请求拦截器
 axios.interceptors.request.use(
   (config) => {
+    NProgress.start()
     // 每次发送请求之前判断是否存在token，如果存在，则统一在http请求的header都加上token，不用每次请求都手动添加了
     // 即使本地存在token，也有可能token是过期的，所以在响应拦截器中要对返回状态进行判断
     // userStore在beforeEach中就创建成功
@@ -24,6 +29,7 @@ axios.interceptors.request.use(
 // 响应拦截器
 axios.interceptors.response.use(
   (response) => {
+    NProgress.done()
     if (response.status === 200) {
       return Promise.resolve(response)
     } else {

45fd0b93d9a99f1fac8c389f34e3f42e3748a9a4
Hinoki <yukihinoki@qq.com>
2022-03-08 16:42:51 +0800

    feat: add env variable&perf echarts

--- vite.config.ts
+++ vite.config.ts
@@ -1,73 +1,83 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig, loadEnv, optimizeDeps } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path'
 import styleImport from 'vite-plugin-style-import'
 import ViteCompression from 'vite-plugin-compression'
+import visualizer from 'rollup-plugin-visualizer'
+// import { loadEnv } from './config/load-env'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    ViteCompression({
-      verbose: true,
-      disable: false,
-      threshold: 10240,
-      algorithm: 'gzip',
-      ext: '.gz',
-    }),
-    styleImport({
-      libs: [
-        {
-          libraryName: '@fect-ui/vue',
-          esModule: true,
-          resolveStyle: (name) => {
-            if (name === 'form') {
-              return
-            } else {
-              return `@fect-ui/vue/es/${name}/index.css`
-            }
+// @ts-ignore
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, __dirname)
+  return {
+    base: '/admin/',
+    plugins: [
+      vue(),
+      visualizer(),
+      ViteCompression({
+        verbose: true,
+        disable: false,
+        threshold: 10240,
+        algorithm: 'gzip',
+        ext: '.gz',
+      }),
+      styleImport({
+        libs: [
+          {
+            libraryName: '@fect-ui/vue',
+            esModule: true,
+            resolveStyle: (name) => {
+              if (name === 'form') {
+                return
+              } else {
+                return `@fect-ui/vue/es/${name}/index.css`
+              }
+            },
+          },
+        ],
+      }),
+    ],
+    resolve: {
+      alias: [{ find: '@admin', replacement: resolve(__dirname, 'src') }],
+    },
+    // less
+    css: {
+      preprocessorOptions: {
+        less: {
+          javascriptEnabled: true,
+        },
+      },
+    },
+    build: {
+      terserOptions: {
+        compress: {
+          // move console.log
+          drop_console: true,
+          drop_debugger: true,
+        },
+      },
+      assetsDir: 'static/img/',
+      rollupOptions: {
+        output: {
+          chunkFileNames: 'static/js/[name]-[hash].js',
+          entryFileNames: 'static/js/[name]-[hash].js',
+          assetFileNames: 'static/[ext]/[name]-[hash].[ext]',
+          manualChunks: {
+            echarts: ['echarts'],
           },
         },
-      ],
-    }),
-  ],
-  resolve: {
-    alias: [{ find: '@admin', replacement: resolve(__dirname, 'src') }],
-  },
-  // less
-  css: {
-    preprocessorOptions: {
-      less: {
-        javascriptEnabled: true,
       },
     },
-  },
-  build: {
-    terserOptions: {
-      compress: {
-        // move console.log
-        drop_console: true,
-        drop_debugger: true,
+    // config proxy
+    server: {
+      proxy: {
+        '/api': {
+          target: 'http://localhost:4000',
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ''),
+        },
       },
     },
-    assetsDir: 'static/img/',
-    rollupOptions: {
-      output: {
-        chunkFileNames: 'static/js/[name]-[hash].js',
-        entryFileNames: 'static/js/[name]-[hash].js',
-        assetFileNames: 'static/[ext]/[name]-[hash].[ext]',
-      },
-    },
-  },
-  // config proxy
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:4000',
-        changeOrigin: true,
-        // /api/todo -> http://localhost:4000/todo
-        rewrite: (path) => path.replace(/^\/api/, ''),
-      },
-    },
-  },
+  }
 })

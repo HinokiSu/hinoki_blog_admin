e43156d84f83798df963875a7b2df8f1a7cf5ac5
Hinoki <yukihinoki@qq.com>
2022-03-17 22:46:20 +0800

    feat: add method of get pageviews& top article

--- src/stores/fettleStore.ts
+++ src/stores/fettleStore.ts
@@ -1,13 +1,18 @@
+import { IFettle, IHttpFettle, IHttpTopArticle, TopArticle } from '@admin/interfaces'
 import { httpGet } from '@admin/utils/axios'
 import { defineStore } from 'pinia'
 
 interface IState {
   fettleList: IFettle[]
+  pageviewsList: any
+  topArticleList: TopArticle[]
 }
 
 export const useFettleStore = defineStore('fettle', {
   state: (): IState => ({
     fettleList: [],
+    pageviewsList: [],
+    topArticleList: [],
   }),
 
   getters: {},
@@ -15,6 +20,22 @@ export const useFettleStore = defineStore('fettle', {
     async getArticleCountInCategory() {
       const res = <IHttpFettle>await httpGet({ url: '/state/category' })
       this.fettleList = res.fettle
+    },
+
+    async getTopArticles() {
+      const res = <IHttpTopArticle>await httpGet({ url: '/state/article/top' })
+      this.topArticleList = res.fettle
+    },
+
+    async getPageviewsByCategory() {
+      const res = <IHttpFettle>await httpGet({ url: '/state/pageviews/category' })
+      const { fettle } = res
+      // {name: , value: } -> [ , ]
+      const temp = []
+      for (let t of fettle) {
+        temp.push([t.name, t.value])
+      }
+      this.pageviewsList = temp
     },
   },
 })
--- src/stores/index.ts
+++ src/stores/index.ts
@@ -1,3 +1,4 @@
 export * from './articleStore'
 export * from './categoryStore'
 export * from './userStore'
+export * from './fettleStore'

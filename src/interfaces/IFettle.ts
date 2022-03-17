efdd17cb9f27aac48b9859b06e2c374fd9dc482c
Hinoki <yukihinoki@qq.com>
2022-03-17 22:47:02 +0800

    chore: modify fettle interfaces

--- src/interfaces/IFettle.ts
+++ src/interfaces/IFettle.ts
@@ -1,9 +1,20 @@
-interface IHttpFettle {
+export interface IHttpFettle {
   message: string
   fettle: IFettle[]
 }
 
-interface IFettle {
+export interface IFettle {
   value: number
   name: string
+}
+
+export type TopArticle = {
+  _id: string
+  title: string
+  totalVisits: number
+}
+
+export interface IHttpTopArticle {
+  message: string
+  fettle: TopArticle[]
 }
--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -1,3 +1,4 @@
 export * from './IBreadNav'
 export * from './IArticle'
 export * from './IUser'
+export * from './IFettle'

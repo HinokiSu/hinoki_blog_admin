9646241d75446cc3fbd048bf9db99e594ba5ad67
Hinoki <yukihinoki@qq.com>
2022-03-17 22:47:41 +0800

    chore: add bar chart

--- src/plugins/echarts.ts
+++ src/plugins/echarts.ts
@@ -1,8 +1,33 @@
-import { TitleComponent, TooltipComponent, VisualMapComponent } from 'echarts/components'
-import { PieChart } from 'echarts/charts'
+import {
+  TitleComponent,
+  TooltipComponent,
+  VisualMapComponent,
+  DatasetComponent,
+  GridComponent,
+  TransformComponent,
+  DatasetComponentOption,
+  GridComponentOption,
+  LegendComponent,
+} from 'echarts/components'
+import { PieChart, BarChart, BarSeriesOption } from 'echarts/charts'
 import { LabelLayout } from 'echarts/features'
 import { CanvasRenderer } from 'echarts/renderers'
+import * as echarts from 'echarts/core'
 
-export const Charts = [TitleComponent, TooltipComponent, VisualMapComponent, PieChart, LabelLayout, CanvasRenderer]
+export const Charts = [
+  PieChart,
+  TitleComponent,
+  TooltipComponent,
+  VisualMapComponent,
+  LabelLayout,
+  CanvasRenderer,
+  BarChart,
+  DatasetComponent,
+  GridComponent,
+  TransformComponent,
+  LegendComponent,
+]
+
+export type BarEChartsOption = echarts.ComposeOption<DatasetComponentOption | GridComponentOption | BarSeriesOption>
 
 export const ChartRegister = (instance: any) => Charts.map((_) => instance.use(_))

c3dbcaefb6c2dacf52430927136f81cdc57e9921
Hinoki <yukihinoki@qq.com>
2022-03-18 16:41:42 +0800

    feat: use pageviews and top article components

--- src/views/fettle/index.vue
+++ src/views/fettle/index.vue
@@ -1,17 +1,23 @@
 <template>
   <div class="hinoki-blog states">
     <article-pie></article-pie>
+    <pageviews-bar></pageviews-bar>
+    <top-article></top-article>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
 import ArticlePie from '@admin/components/charts/article-pie.vue'
+import PageviewsBar from '@admin/components/charts/pageviews-bar.vue'
+import TopArticle from '@admin/components/charts/top-article.vue'
 export default defineComponent({
   name: 'Fettle',
   components: {
     ArticlePie,
-  },
+    PageviewsBar,
+    TopArticle
+},
   setup() {
     return {}
   },
